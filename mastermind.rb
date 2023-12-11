d3a4337f36fe24805c1e7885105ceafd99c8e0d9
sdc9600 <149299980+sdc9600@users.noreply.github.com>
2023-12-11 22:06:08 +1030

    Non-functional comp guess, refactoring needed

--- mastermind.rb
+++ mastermind.rb
@@ -46,12 +46,15 @@ class MastermindHumanBreaker
 end
 
 class MastermindComputerBreaker
-  attr_reader :set_of_guesses
+  attr_reader :set_of_guesses, :turn, :black_pegs
   def initialize
     @human_code = ''
     @turn = 1
     @colours = ['B','G','R','Y','O','V']
     @set_of_guesses = []
+    @computer_guess = ['B','B','G','G']
+    @black_pegs = 0
+    @white_pegs = 0
   end
 
   def computer_set
@@ -60,7 +63,8 @@ class MastermindComputerBreaker
     k = 0
     l = 0
     until @set_of_guesses.length == 1296
-    @set_of_guesses.append(@colours[i]+@colours[j]+@colours[k]+@colours[l])
+      append_value = (@colours[i]+@colours[j]+@colours[k]+@colours[l]).chars
+    @set_of_guesses.append(append_value)
     if l < 5
       l += 1
     elsif k < 5
@@ -80,29 +84,123 @@ class MastermindComputerBreaker
 end
 
   def human_set_code
-    puts "Please enter a four character string containing a combination of 'b, g, r, y, o, v to guess the secret code!"
+    puts "Please enter a four character string containing a combination of 'b, g, r, y, o, v to set the secret code!"
     until @human_code.length == 4 && @human_code.scan(/[BGRYOV]/).join.length == 4
       @human_code = gets.chomp.upcase
     end
     @human_code = @human_code.chars
   end
+
+  def computer_codebreaker
+    if @turn == 1
+      self.evaluate_computer_guess(@computer_guess)
+    end
+    @computer_guess = @set_of_guesses.sample
+    evaluate_computer_guess(@computer_guess)
+  end
+
+  def pop_from_set
+    @set_of_guesses.each_index do
+      |i|
+    direct_hit = 0
+    indirect_hit = 0
+      @computer_guess.each_index do
+        |index|
+        if @computer_guess[index] == @set_of_guesses[i][index]
+          direct_hit += 1
+        elsif @computer_guess[index].include? @set_of_guesses[i].join
+        indirect_hit += 1
+      end
+    end
+      
+      if @black_pegs == 0 && @white_pegs == 0 && direct_hit != 0
+        @set_of_guesses.delete(@set_of_guesses[i])
+        i -= 1
+      elsif @black_pegs == 1 && @white_pegs == 0 && direct_hit != 1
+        @set_of_guesses.delete(@set_of_guesses[i])
+        i -= 1
+      elsif @black_pegs == 2 && @white_pegs == 0 && direct_hit != 2
+        @set_of_guesses.delete(@set_of_guesses[i])
+        i -= 1
+      elsif @black_pegs == 3 && direct_hit != 3
+        @set_of_guesses.delete(@set_of_guesses[i])
+        i -= 1
+      elsif @black_pegs == 0 && @white_pegs == 1 && indirect_hit != 1
+        @set_of_guesses.delete(@set_of_guesses[i])
+        i -= 1
+      elsif @black_pegs == 0 && @white_pegs == 2 && indirect_hit != 2
+        @set_of_guesses.delete(@set_of_guesses[i])
+        i -= 1
+      elsif @black_pegs == 0 && @white_pegs == 3 && indirect_hit != 3
+        @set_of_guesses.delete(@set_of_guesses[i])
+        i -= 1
+      elsif @black_pegs == 0 && @white_pegs == 4 && indirect_hit != 4
+        @set_of_guesses.delete(@set_of_guesses[i])
+        i -= 1
+      elsif @black_pegs == 1 && @white_pegs == 1 && (direct_hit + indirect_hit) != 2
+        @set_of_guesses.delete(@set_of_guesses[i])
+        i -= 1
+      elsif @black_pegs == 1 && @white_pegs == 2 && (direct_hit + indirect_hit) != 3
+        @set_of_guesses.delete(@set_of_guesses[i])
+        i -= 1
+      elsif @black_pegs == 1 && @white_pegs == 3 && (direct_hit + indirect_hit) != 4
+        @set_of_guesses.delete(@set_of_guesses[i])
+        i -= 1
+      elsif @black_pegs == 2 && @white_pegs == 2 && (direct_hit + indirect_hit) != 4
+        @set_of_guesses.delete(@set_of_guesses[i])
+        i -= 1
+      end 
+    end
+    puts @set_of_guesses.length
+  end
+
+  def evaluate_computer_guess(guess)
+  incorrect_pegs = 0
+  @black_pegs = 0
+  @white_pegs = 0
+  guess.each_index do 
+    |i|
+    if   
+    guess[i] == @human_code[i]
+      @black_pegs += 1
+    elsif
+      guess.include? @human_code[i]
+        @white_pegs +=1
+    else
+      incorrect_pegs += 1
+    end
+  end
+  puts "\nYour guess contains #{@black_pegs} colours in the right posiiton, 
+  #{@white_pegs} colours in the incorrect position
+  and #{incorrect_pegs} colours not existing in the code!\n"
+  @turn +=1
+  end
 end
 
 game = MastermindComputerBreaker.new
+game.computer_set
 game.human_set_code
-print game.computer_set
-print game.set_of_guesses.length
 
-
-
-
-if 1==0
-game = MastermindHumanBreaker.new
-game.generate_computer_code
-
-
-while game.turn < 13
-  game.human_guess_code
-  game.evaluate_guess
+while game.turn < 20
+game.computer_codebreaker
+puts game.black_pegs
+if game.black_pegs == 4
+  puts "You've guessed the code correctly and win!"
+  break 
 end
+game.pop_from_set
+
 end
+
+
+
+
+
+#game = MastermindHumanBreaker.new
+#game.generate_computer_code
+
+
+#while game.turn < 13
+ # game.human_guess_code
+ # game.evaluate_guess
+#end
